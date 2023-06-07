5fbafabaa48cf74d0f98da727012025615f86224
Noitabara <me@noita.dev>
2023-06-07 03:39:34 -0400

    remove build, update package.json

--- build.js
+++
@@ -1,9 +0,0 @@
-// General usage
-const { build } = require("esbuild");
-const esbuildPluginPino = require("esbuild-plugin-pino");
-
-build({
-  entryPoints: ["src/index.ts"],
-  outdir: "dist",
-  plugins: [esbuildPluginPino({ transports: ["pino-pretty"] })],
-}).catch(() => process.exit(1));
